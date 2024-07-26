ca0afd9072a9a88708b22976c4a0483575a8477b
kamil.szerlag <kamil.szerlag>
2024-07-26 14:39:20 +0200

    initial files with eslint and prettier

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Financial Empire</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
