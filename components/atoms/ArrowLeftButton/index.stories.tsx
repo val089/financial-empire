c89826de1435809d5ade6fa7c1bd8e96e553cb95
kamil.szerlag <kamil.szerlag>
2024-12-13 11:20:12 +0100

    6-feat/add-storybook

---
+++ components/atoms/ArrowLeftButton/index.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ArrowLeftButton from '.';
+
+const meta = {
+  title: 'Atoms/ArrowLeftButton',
+  component: ArrowLeftButton,
+  argTypes: {},
+  args: {
+    onPress: () => console.log('button pressed'),
+  },
+  // decorators: [
+  //   (Story) => (
+  //     <View style={{ padding: 16, alignItems: 'flex-start' }}>
+  //       <Story />
+  //     </View>
+  //   ),
+  // ],
+} satisfies Meta<typeof ArrowLeftButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {};
