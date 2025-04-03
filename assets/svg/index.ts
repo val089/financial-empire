8c3535f7cf2b863c742da64c28159ce37f571398
kamil.szerlag <kamil.szerlag>
2025-04-03 19:14:39 +0200

    13-feat:add button to add entry

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,5 +1,6 @@
 import IconArrowLeft from './arrow-left.svg';
 import IconEyeOpen from './eye-open.svg';
 import IconEyeClose from './eye-close.svg';
+import IconPlus from './plus.svg';
 
-export { IconArrowLeft, IconEyeClose, IconEyeOpen };
+export { IconArrowLeft, IconEyeClose, IconEyeOpen, IconPlus };
--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,5 +2,6 @@ import Avatar from './Avatar';
 import ArrowLeftButton from './ArrowLeftButton';
 import Input from './fields/Input';
 import PasswordInput from './fields/PasswordInput';
+import FloatingAddButton from './FloatingAddButton';
 
-export { Avatar, ArrowLeftButton, Input, PasswordInput };
+export { Avatar, ArrowLeftButton, Input, PasswordInput, FloatingAddButton };
