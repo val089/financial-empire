158eae1e3bf88a99b61251171bfbdb11ede6bb6c
kamil.szerlag <kamil.szerlag>
2025-07-06 19:20:54 +0200

    fix timeout

--- hooks/useRefreshOnScroll/index.tsx
+++ hooks/useRefreshOnScroll/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { RefreshControl } from 'react-native';
 import { QueryObserverResult } from '@tanstack/react-query';
 
@@ -8,20 +8,31 @@ const useRefreshOnScroll = ({
   refetch: () => Promise<QueryObserverResult>;
 }) => {
   const [refreshing, setRefreshing] = useState(false);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   const handleRefresh = async () => {
     setRefreshing(true);
     await refetch();
-    setTimeout(() => {
+    timeoutRef.current = setTimeout(() => {
       setRefreshing(false);
     }, 500);
   };
+
+  useEffect(
+    () => () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+    },
+    []
+  );
 
   const refreshControl = (
     <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
   );
 
-  return { refreshing, refreshControl };
+  return { refreshControl };
 };
 
 export default useRefreshOnScroll;
