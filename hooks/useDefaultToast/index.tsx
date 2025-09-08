ba63b6c909dc2678ecbc5840eaebbef4d6cdb75c
kamil.szerlag <kamil.szerlag>
2025-09-08 13:04:36 +0200

    add todo

--- hooks/useDefaultToast/index.tsx
+++ hooks/useDefaultToast/index.tsx
@@ -1,6 +1,7 @@
 import Toast from 'react-native-toast-message';
 
 const useDefaultToast = () => {
+  // TOOD: improve this hook to support more customization
   const showErrorToast = (message?: string, title?: string) =>
     Toast.show({
       type: 'error',
@@ -9,6 +10,8 @@ const useDefaultToast = () => {
         message ||
         'Something was wrong. Please try again or contact the administrator if the error occurs again.',
       position: 'top',
+      // text1Style: { fontSize: 14 },
+      // text2Style: { fontSize: 12 },
     });
 
   const showSuccessToast = (message: string) =>
