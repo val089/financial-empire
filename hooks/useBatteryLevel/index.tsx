b1bba4e4cc2cc53cfeefc8bdd49d7ce1f4c88bed
kamil.szerlag <kamil.szerlag>
2025-09-12 10:32:51 +0200

    set displaying of battery status only on phisical device

--- hooks/useBatteryLevel/index.tsx
+++ hooks/useBatteryLevel/index.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from 'react';
 import { NativeModules } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 const { BatteryModule } = NativeModules;
+
+// i want use this hook only on phiysical devices, write some condition to check if the app is running on a simulator or emulator
+// you can use the 'react-native-device-info' library to check if the device is a simulator or emulator
+// import DeviceInfo from 'react-native-device-info';
+// const isEmulator = DeviceInfo.isEmulatorSync();
+// if (isEmulator) {
+//   console.warn('useBatteryLevel hook is not supported on simulators/emulators.');
+//   return null;
+// }
 
 // Native modules work only on real devices, not on simulators/emulators
 
@@ -19,6 +29,13 @@ const useBatteryLevel = (pollingInterval: number = 5000) => {
   };
 
   useEffect(() => {
+    if (DeviceInfo.isEmulatorSync()) {
+      console.warn(
+        'useBatteryLevel hook is not supported on simulators/emulators.'
+      );
+      return;
+    }
+
     fetchBatteryLevel();
     const interval = setInterval(fetchBatteryLevel, pollingInterval);
 
